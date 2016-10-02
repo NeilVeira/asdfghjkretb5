9ded26f25c6be15faccd0a1be737dd6e4833e5fd
NeilVeira <neil.veira@mail.utoronto.ca>
2016-10-01 17:55:45 -0700

    Add home page controller and route the root to its index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'home_page/index'
+
+  root 'home_page#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
