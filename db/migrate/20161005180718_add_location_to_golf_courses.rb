2bd9fc5214b35111acbe3d47e97f44389d08cbc9
NeilVeira <neil.veira@mail.utoronto.ca>
2016-10-10 20:26:42 -0700

    Delete unneeded migrations

--- db/migrate/20161005180718_add_location_to_golf_courses.rb
+++
@@ -1,5 +0,0 @@
-class AddLocationToGolfCourses < ActiveRecord::Migration[5.0]
-  def change
-    add_column :golf_courses, :location, :string
-  end
-end
--- db/migrate/20161008031356_drop_registered_people.rb
+++
@@ -1,5 +0,0 @@
-class DropRegisteredPeople < ActiveRecord::Migration[5.0]
-  def change
-		drop_table :registered_people
-  end
-end
