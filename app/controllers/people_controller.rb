fdd3867caca37397ed0170ca0370699038ca7610
NeilVeira <neil.veira@mail.utoronto.ca>
2016-10-10 20:22:04 -0700

    Make index, update, and delete actions and their associated views for people controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,25 +1,41 @@
 class PeopleController < ApplicationController
-    def new
-    end
-    
-    def create
-        @person = Person.new(params.require(:person).permit(:firstname, :lastname, :dob, :phone, :email))
-        @person.save
-        redirect_to @person
-        
-        #TODO:
-        #   - add password
-        #   - Verify that all fields are valid
-        #   - check that (name,password) key doesn't exist in db already?
-
-    end
-    
-    def show
-        @person = Person.find(params[:id])
-    end
+	def index
+		@people = Person.all
+	end
+	
+	def new
+	end
+	
+	def create
+		@person = Person.new(person_params)
+		@person.save
+		redirect_to @person
+	end
+	
+	def show
+		@person = Person.find(params[:id])
+	end
+	
+	def edit
+		@person = Person.find(params[:id])
+	end
+	
+	def update
+		@person = Person.find(params[:id])
+		@person.update(person_params)
+		redirect_to @person
+	end
+	
+	def destroy
+		@person = Person.find(params[:id])
+		@person.destroy
+		redirect_to people_path
+	end
+	
+	private
+		def person_params
+			params.require(:person).permit(:firstname, :lastname, :dob, :phone, :location, :email)
+		end
+	
 end
 
-#Notes:
-#   - Currently no index or destroy actions - not needed?
-#TODO:
-#   - create sign-in and sign-out actions
