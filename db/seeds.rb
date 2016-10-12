d2fe805f1d47d284d5eae96d3f2ff9a70d67ed76
Fang Bei Bay Zhang <bay.zhang@mail.utoronto.ca>
2016-10-11 20:59:26 -0400

    updating seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 people_list = [
-	["FirstName1", "LastName1", "dob1", "phone1","email1"],["FirstName2", "LastName2", "dob2",  "phone2","email2"],["FirstName3", "LastName3", "dob3", "address3", "phone3","email3"],
-["FN3", "LN3", "dob3",  "phone3","email3"],["FN4", "LN4", "dob4",  "phone4","email4"],["FN5", "LN5", "d5",  "ph5","em5"],["FN6", "LN6", "d6",  "ph6","em6"]
+	["FirstName1", "LastName1", "dob1", "phone1","email1","loc1"],["FirstName2", "LastName2", "dob2",  "phone2","email2","loc2"],["FirstName3", "LastName3", "dob3", "address3", "phone3","email3","loc3"],
+["FN3", "LN3", "dob3",  "phone3","email3","loc4"],["FN4", "LN4", "dob4",  "phone4","email4","loc5"],["FN5", "LN5", "d5",  "ph5","em5","loc7"],["FN6", "LN6", "d6",  "ph6","em6","loc8"]
 ]
 
-people_list.each do |firstname,lastname,dob,phone,email|
-	Person.create(firstname: firstname, lastname: lastname, dob: dob, phone: phone, email: email)
+people_list.each do |firstname,lastname,dob,phone,email,location|
+	Person.create(firstname: firstname, lastname: lastname, dob: dob, phone: phone, email: email, location: location)
 end
 
 golfcourse_list = [
-	"golfn1","golfn2","golfn3","golfn4","golfn5","golfn6","golfn7"
+	["golfn1","loc1"],["golfn2","loc2"],["golfn3","loc3"],["golfn4","loc4"],["golfn5","loc5"],["golfn6","loc6"],["golfn7","loc7"]
 ]
 
-golfcourse_list.each do |name|
-	GolfCourse.create(name: name)
+golfcourse_list.each do |name,location|
+	GolfCourse.create(name: name, location: location)
 end
 
 tournaments_list = [
@@ -45,14 +45,6 @@ tickets_list = [
 
 tickets_list.each do |tickettype, person_id|
 	Ticket.create(tickettype: tickettype,person_id: person_id )
-end
-
-registered_people_list = [
-	[Person.first.id,Tournament.first.id],[Person.second.id,Tournament.first.id],[Person.third.id,Tournament.third.id],[Person.fourth.id,Tournament.first.id]
-]
-
-registered_people_list.each do | person_id, tournament_id|
-	RegisteredPerson.create(person_id: person_id, tournament_id: tournament_id)
 end
 
 registered_team_list = [
