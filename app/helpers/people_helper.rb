91013edec0eb4b01bd0ce8a2c1a683ac26513ae1
Cathy <cathyyfu@gmail.com>
2016-10-25 14:00:12 -0400

    modified people_helper functions

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,23 +1,20 @@
 module PeopleHelper
+
   private
-  def get_all_sponsors
-    @person = current_person
-    Sponsor.where(person_id: @person.id)
+  def get_all_sponsors (person)
+    Sponsor.where(person_id: person.id)
   end
 
-  def get_all_players
-    @person = current_person
-    Player.where(person_id: @person.id)
+  def get_all_players (person)
+    Player.where(person_id: person.id)
   end
 
-  def get_all_tournament_organizers
-    @person = current_person
-    TournamentOrganizer.where(person_id: @person.id)
+  def get_all_tournament_organizers (person)
+    TournamentOrganizer.where(person_id: person.id)
   end
 
-  def get_all_golf_course_organizers
-    @person = current_person
-    GolfCourseOrganizer.where(person_id: @person.id)
+  def get_all_golf_course_organizers (person)
+    GolfCourseOrganizer.where(person_id: person.id)
   end
 
   def find_tournaments_for (listOfPeople)
@@ -27,7 +24,7 @@ module PeopleHelper
       @tournamentIds << person.tournament.id
     end
 
-    return Tournament.where(id: @tournamentIds).order(sort_column + " " + sort_direction)
+    Tournament.where(id: @tournamentIds).order(sort_column + " " + sort_direction)
   end
 
   def find_golf_courses_for (listOfPeople)
@@ -37,27 +34,27 @@ module PeopleHelper
       golfCourseIds << person.golf_course.id
     end
 
-    return GolfCourse.where(id: @golfCourseIds).order(sort_column + " " + sort_direction)
+    GolfCourse.where(id: @golfCourseIds).order(sort_column + " " + sort_direction)
   end
 
   public
   # returns tournament objects
-  def get_all_tournaments_as (peopleType)
+  def get_all_tournaments_as (peopleType, personToFind = current_person) #default arg
     case peopleType
       when "sponsor"
-          return find_tournaments_for (get_all_sponsors)
+          return find_tournaments_for (get_all_sponsors personToFind)
       when "player"
-          return find_tournaments_for (get_all_players)
+          return find_tournaments_for (get_all_players personToFind)
       when "tournament_organizer", "organizer"
-          return find_tournaments_for (get_all_tournament_organizers)
+          return find_tournaments_for (get_all_tournament_organizers personToFind)
     end
   end
 
   # returns golf course objects
-  def get_all_golf_courses_as (peopleType)
+  def get_all_golf_courses_as (peopleType, personToFind = current_person) #default arg
     case peopleType
       when "organizer", "golf_course_organizer"
-        return find_golf_courses_for (get_all_golf_course_organizers)
+        return find_golf_courses_for (get_all_golf_course_organizers personToFind)
     end
   end
 end
