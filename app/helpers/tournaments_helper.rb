8f7c96d91bb87d68d4ebccc1670491d05ed2918e
NeilVeira <neil.veira@mail.utoronto.ca>
2016-10-29 16:34:10 -0400

    When showing a tournament give link to register if current user is not already registered

--- app/helpers/tournaments_helper.rb
+++ app/helpers/tournaments_helper.rb
@@ -1,2 +1,18 @@
 module TournamentsHelper
+
+	def user_is_registered?(tournament_id)
+		if user_signed_in?
+			@person = current_person
+			@player = Player.find_by tournament_id: tournament_id, person_id: @person.id
+			@tournament_organizer = TournamentOrganizer.find_by tournament_id: tournament_id, person_id: @person.id
+			@sponsor = Sponsor.find_by tournament_id: tournament_id, person_id: @person.id
+			if @player or @tournament_organizer or @sponsor
+				return true
+			else
+				return false
+			end
+		else
+			return false
+		end
+	end
 end
