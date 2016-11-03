59c4c40c92bec9e43308226b073bb37e1c645ef2
Cathy <cathyyfu@gmail.com>
2016-11-03 00:01:01 -0400

    added price to tournaments table for spectator and player (registration price, etc)

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161030174223) do
+ActiveRecord::Schema.define(version: 20161103035614) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "addressType"
@@ -126,10 +125,12 @@ ActiveRecord::Schema.define(version: 20161030174223) do
     t.text     "description"
     t.boolean  "ispublic"
     t.text     "extrafeatures"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
     t.integer  "golf_course_id"
     t.datetime "date"
+    t.decimal  "price_player",    precision: 6, scale: 2
+    t.decimal  "price_spectator", precision: 6, scale: 2
     t.index ["golf_course_id"], name: "index_tournaments_on_golf_course_id"
   end
 
@@ -159,157 +160,3 @@ ActiveRecord::Schema.define(version: 20161030174223) do
   end
 
 end
-=======
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 20161024222501) do
-
-  create_table "addresses", force: :cascade do |t|
-    t.string   "addressType"
-    t.integer  "apartmentNumber"
-    t.integer  "streetNumber"
-    t.string   "streetName"
-    t.string   "city"
-    t.string   "province"
-    t.string   "country"
-    t.string   "postalCode"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  create_table "golf_course_organizers", force: :cascade do |t|
-    t.integer  "person_id"
-    t.integer  "adminrights"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.integer  "golf_course_id"
-    t.index ["golf_course_id"], name: "index_golf_course_organizers_on_golf_course_id"
-    t.index ["person_id"], name: "index_golf_course_organizers_on_person_id"
-  end
-
-  create_table "golf_courses", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "address_id"
-    t.index ["address_id"], name: "index_golf_courses_on_address_id"
-  end
-
-  create_table "people", force: :cascade do |t|
-    t.string   "firstname"
-    t.string   "lastname"
-    t.string   "dob"
-    t.string   "phone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "user_id"
-    t.integer  "address_id"
-    t.index ["address_id"], name: "index_people_on_address_id"
-    t.index ["user_id"], name: "index_people_on_user_id"
-  end
-
-  create_table "players", force: :cascade do |t|
-    t.integer  "person_id"
-    t.integer  "tournament_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.index ["person_id"], name: "index_players_on_person_id"
-    t.index ["tournament_id"], name: "index_players_on_tournament_id"
-  end
-
-  create_table "registered_teams", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "person_id"
-    t.integer  "tournament_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.index ["person_id"], name: "index_registered_teams_on_person_id"
-    t.index ["tournament_id"], name: "index_registered_teams_on_tournament_id"
-  end
-
-  create_table "sponsors", force: :cascade do |t|
-    t.integer  "person_id"
-    t.integer  "tournament_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.index ["person_id"], name: "index_sponsors_on_person_id"
-    t.index ["tournament_id"], name: "index_sponsors_on_tournament_id"
-  end
-
-  create_table "teams", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "tournament_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.index ["tournament_id"], name: "index_teams_on_tournament_id"
-  end
-
-  create_table "tickets", force: :cascade do |t|
-    t.integer  "tickettype"
-    t.integer  "person_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.integer  "tournament_id"
-    t.index ["person_id"], name: "index_tickets_on_person_id"
-    t.index ["tournament_id"], name: "index_tickets_on_tournament_id"
-  end
-
-  create_table "tournament_organizers", force: :cascade do |t|
-    t.integer  "person_id"
-    t.integer  "tournament_id"
-    t.integer  "adminrights"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.index ["person_id"], name: "index_tournament_organizers_on_person_id"
-    t.index ["tournament_id"], name: "index_tournament_organizers_on_tournament_id"
-  end
-
-  create_table "tournaments", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.boolean  "ispublic"
-    t.text     "extrafeatures"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.integer  "golf_course_id"
-    t.datetime "date"
-    t.index ["golf_course_id"], name: "index_tournaments_on_golf_course_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "website_admins", force: :cascade do |t|
-    t.integer  "person_id"
-    t.integer  "adminrights"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["person_id"], name: "index_website_admins_on_person_id"
-  end
-
-end
->>>>>>> b471fb4664a9c36e7f47767ba12fecc85963365a
