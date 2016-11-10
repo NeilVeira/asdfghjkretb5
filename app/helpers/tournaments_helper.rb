aeaffd3a22ba4f70697686c630b6fad3dbc9b430
Cathy <cathyyfu@gmail.com>
2016-11-10 00:44:21 -0500

    changed check_status to take in list of tournament arguments again

--- app/helpers/tournaments_helper.rb
+++ app/helpers/tournaments_helper.rb
@@ -1,7 +1,7 @@
 module TournamentsHelper
 
 	def user_is_registered?(tournament)
-		status = check_status()
+		status = check_status(tournament)
 		if status[tournament.id]
 			return true
 		else 
@@ -9,17 +9,16 @@ module TournamentsHelper
 		end
 	end
 
-	def check_status()
-		tournaments = Tournament.all
+	def check_status(tournaments)
 		status = Hash.new
-		person = current_person
+		@p = current_person
 		tournaments.each do |t|
 			logger.debug "tournament: #{t.id}"
-			if Ticket.find_by_tournament_id_and_person_id(t.id, person.id)
+			if @p and Ticket.find_by_tournament_id_and_person_id(t.id, @p.id)
 				status[t.id] = 1
 			end
 		end
-
 		return status
 	end
+
 end
