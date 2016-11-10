f97dbcc9e83ceee1069fe1bfec8eb7f1687695d2
Cathy <cathyyfu@gmail.com>
2016-11-09 20:20:20 -0500

    moved check_status function

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -5,7 +5,6 @@ class TournamentsController < ApplicationController
 
 	def index
 		@tournaments = Tournament.order(sort_column + " " + sort_direction)
-		@status = check_status(@tournaments)
 	end
   
 	def new
@@ -52,17 +51,5 @@ class TournamentsController < ApplicationController
 	def sort_column
 		Tournament.column_names.include?(params[:sort]) ? params[:sort] : "id"
 	end
-	
-	def check_status(tournaments)
-		status = Hash.new
-		@p = current_person
-		tournaments.each do |t|
-			logger.debug "tournament: #{t.id}"
-			if Ticket.find_by_tournament_id_and_person_id(t.id, @p.id)
-				status[t.id] = 1
-			end		
-		end
-		
-		return status
-	end
+
 end
--- app/helpers/tournaments_helper.rb
+++ app/helpers/tournaments_helper.rb
@@ -15,4 +15,19 @@ module TournamentsHelper
 			return false
 		end
 	end
+
+	def check_status()
+		tournaments = Tournament.all
+		status = Hash.new
+		person = current_person
+		tournaments.each do |t|
+			logger.debug "tournament: #{t.id}"
+			if Ticket.find_by_tournament_id_and_person_id(t.id, person.id)
+				status[t.id] = 1
+			end
+		end
+
+		return status
+	end
+
 end
