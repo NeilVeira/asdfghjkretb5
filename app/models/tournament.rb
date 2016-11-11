f83b031e5066bb92ff8b1458554240e3a7b0a5f8
NeilVeira <neil.veira@mail.utoronto.ca>
2016-11-11 15:53:59 -0500

    Update model validation

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -7,8 +7,8 @@ class Tournament < ApplicationRecord
 
 	validates :name, presence: true 
 	validates :description, presence: true
-	validates :pricePlayer, allow_blank: true, format: { with: /\A (\d{1,4}\.\d{2}) {1}\z/}
-	validates :priceSpectator, allow_blank: true, format: { with: /\A (\d{1,4}\.\d{2}) {1}\z/}
+	#validates :pricePlayer, allow_blank: true, format: { with: /\A (\d{1,4}\.\d{2}) {1}\z/}
+	#validates :priceSpectator, allow_blank: true, format: { with: /\A (\d{1,4}\.\d{2}) {1}\z/}
 	
 	validates_inclusion_of :ispublic, :in => [true, false]
 	
