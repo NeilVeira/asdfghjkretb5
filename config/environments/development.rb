38b15afb48ac6b8c7531bd610a397e7e82776249
Aldridge Braz <aldridge.braz@gmail.com>
2016-11-25 19:59:57 -0500

    Added mail server settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,19 @@ Rails.application.configure do
   
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   
+  # mail settings
+	config.action_mailer.raise_delivery_errors = true
+
+	config.action_mailer.delivery_method = :smtp
+
+	config.action_mailer.smtp_settings = {
+	address: "smtp.gmail.com",
+	port: 587,
+	domain: "gmail.com",
+	authentication: "plain",
+	enable_starttls_auto: true,
+	user_name: "andysaladgolferman",
+	password: "golferman"
+	}
+  
 end
