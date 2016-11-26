6c8ad2f86a49d15d0b2c2728a64b25b495dae722
Aldridge Braz <aldridge.braz@gmail.com>
2016-11-25 20:13:17 -0500

    Updated hostname for mail server (Heroku issue)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'test-rubyonrails-app.herokuapp.com/', port: 3000 }
   
   # mail settings
 	config.action_mailer.raise_delivery_errors = true
