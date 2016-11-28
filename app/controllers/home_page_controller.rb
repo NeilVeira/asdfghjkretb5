b4e5cd328fd55eeffcfa68ca73738233a00451f3
Fang Bei Bay Zhang <bay.zhang@mail.utoronto.ca>
2016-11-27 19:03:47 -0500

    Added the main page and golf translations

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,17 +1,4 @@
 class HomePageController < ApplicationController
 	def index
-		if user_signed_in?
-			if user_exists?
-				if user_is_admin?
-					render 'admin_user'
-				else
-					render 'regular_user'
-				end
-			else
-				render 'no_user'
-			end
-		else
-			render 'no_user'
-		end
 	end
 end
