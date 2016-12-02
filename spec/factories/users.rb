30e55632adc2dcfafb95ac189da5042a0009e623
Aldridge Braz <aldridge.braz@gmail.com>
2016-12-02 16:05:55 -0500

    Create new account test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,4 +3,10 @@ FactoryGirl.define do
 		email "1@email.com"
 		password "password"
 	end
+	
+	factory :valid_user, class: User do
+		email "1@email.com"
+		password "password"
+	end
+	
 end
--- spec/features/signin_spec.rb
+++ spec/features/signin_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-feature "signin in" do
-	let(:user) {FactoryGirl.create(:user)}
+feature "signin" do
+	let(:user) {FactoryGirl.create(:valid_user)}
 	
 	def fill_in_signin_fields
 		fill_in "user[email]", with: user.email
@@ -12,13 +12,14 @@ feature "signin in" do
 	scenario "visiting the site to sign in" do
 		visit root_path
 		expect(page).to have_link("Login", visible: false)
-		#find("Login", visible: false).click
 		find('a', :text => "Login", visible: false).click
+		
 		save_and_open_page
 
 		fill_in_signin_fields
-		expect(page).to have_content("Signed in successfully.")
 		
 		save_and_open_page
+		
+		expect(page).to have_content("Signed in successfully.")
 	end
 end
