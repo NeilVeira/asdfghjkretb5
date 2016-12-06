7ae2be6661259db6d943b8d8f9a6589d77428f0a
NeilVeira <neil.veira@mail.utoronto.ca>
2016-12-06 14:09:20 -0500

    Bug fix

--- app/models/tournament_organizer.rb
+++ app/models/tournament_organizer.rb
@@ -3,7 +3,17 @@ class NotRegisteredValidator < ActiveModel::Validator
 		if record.person
 			ticket = Ticket.find_by(person_id: record.person.id, tournament_id: record.tournament.id)
 			if ticket
-				record.errors[:base] << "User #{record.person.user.email} is already registered for this tournament"
+				if ticket.tickettype == 4
+					#Special case when user creates a new tournament, a ticket with tickettype 4 
+					#exists but we still need to create a tournament_organizer.
+					#If no tournament organizer object exists for this user, pass validation
+					to = TournamentOrganizer.find_by(person_id: record.person.id, tournament_id: record.tournament.id)
+					if to
+						record.errors[:base] << "User #{record.person.user.email} is already registered for this tournament"
+					end
+				else
+					record.errors[:base] << "User #{record.person.user.email} is already registered for this tournament"
+				end
 			end
 		else
 			record.errors[:base] << "User not found"
