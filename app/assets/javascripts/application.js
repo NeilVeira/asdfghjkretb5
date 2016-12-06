c3a5708ec42b12604a59e137f32e6c60ca35aded
Aldridge Braz <aldridge.braz@gmail.com>
2016-12-05 22:46:05 -0500

    Removed flex from application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
+
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap
 //= require bootstrap-sprockets
 //= require_tree .
