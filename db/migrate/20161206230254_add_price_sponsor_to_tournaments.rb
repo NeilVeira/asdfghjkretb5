a9f9dd79ce8fc5bfd8193d7a9a2190b2fe847df5
Cathy <cathyyfu@gmail.com>
2016-12-07 01:55:25 -0500

    added currency type to tournaments

--- db/migrate/20161206230254_add_price_sponsor_to_tournaments.rb
+++ db/migrate/20161206230254_add_price_sponsor_to_tournaments.rb
@@ -1,5 +1,6 @@
 class AddPriceSponsorToTournaments < ActiveRecord::Migration[5.0]
   def change
     add_column :tournaments, :priceSponsor, :decimal, precision: 6, scale: 2
+    add_column :tournaments, :currency, :string
   end
 end
