00318425ee2182d360c9c7715a57ac1923e87bbc
NeilVeira <neil.veira@mail.utoronto.ca>
2016-12-06 21:47:45 -0500

    Bug fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@ module ApplicationHelper
 
   def sort(column)
     title = column.titleize
+	column = column.downcase
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
     link_to title, :sort => column, :direction => direction
   end
