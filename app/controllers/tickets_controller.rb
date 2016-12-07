e9369a74666bf9bbddfe8ebae8d4f67a9c975e1e
castielZhu <xiangyu.zhu@mail.utoronto.ca>
2016-12-07 11:30:04 -0500

    add get_ticket_price method in ticket module, recheck price and avalibility before proceed to paypal, change notify address from ngrok to host/paypal_paid, modified routes

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,57 +1,57 @@
 class TicketsController < ApplicationController
-	before_action :authenticate_user!
-	before_action :authenticate_ticket_owner!, only: [:show, :destroy, :payment, :payment_select, :paypal_pay]
-	before_action :authenticate_admin!, only: [:index, :edit, :update]
+  before_action :authenticate_user!
+  before_action :authenticate_ticket_owner!, only: [:show, :destroy, :payment, :payment_select, :paypal_pay]
+  before_action :authenticate_admin!, only: [:index, :edit, :update]
 
     def index
-		
+    
     end
     
     def show
         @ticket = Ticket.find(params[:id])
-		@code = checkIn_url(id:@ticket.id).to_s
-		@qrcode = RQRCode::QRCode.new(@code)
+    @code = checkIn_url(id:@ticket.id).to_s
+    @qrcode = RQRCode::QRCode.new(@code)
     end
     
     def new
-			@ticket = Ticket.new()
-			@tournament = Tournament.find(session[:tournament_id])
-			person = current_person
-			exists_ticket = Ticket.where(tournament_id: session[:tournament_id], person: person).first
-			if exists_ticket
-				redirect_to ticket_path(exists_ticket)
-			end
+      @ticket = Ticket.new()
+      @tournament = Tournament.find(session[:tournament_id])
+      person = current_person
+      exists_ticket = Ticket.where(tournament_id: session[:tournament_id], person: person).first
+      if exists_ticket
+        redirect_to ticket_path(exists_ticket)
+      end
     end
     
     def edit
     end
     
     def create
-			@ticket = create_ticket(ticket_params[:tickettype])
+      @ticket = create_ticket(ticket_params[:tickettype])
 
-			if @ticket # successfully created
-				price = nil
-				case @ticket.tickettype
-					when 1
-						price = @ticket.tournament.pricePlayer
-					when 2
-						price = @ticket.tournament.priceSponsor
-						redirect_to new_sponsor_path
-					when 3
-						price = @ticket.tournament.priceSpectator
-				end
+      if @ticket # successfully created
+        price = nil
+        case @ticket.tickettype
+          when 1
+            price = @ticket.tournament.pricePlayer
+          when 2
+            price = @ticket.tournament.priceSponsor
+            redirect_to new_sponsor_path
+          when 3
+            price = @ticket.tournament.priceSpectator
+        end
 
-				if price != nil and price > 0
-					redirect_to payment_path(@ticket)
-				else
-					@ticket.has_paid = true
-					@ticket.save
-					redirect_to ticket_path(@ticket)
-				end
+        if price != nil and price > 0
+          redirect_to payment_path(@ticket)
+        else
+          @ticket.has_paid = true
+          @ticket.save
+          redirect_to ticket_path(exists_ticket)
+        end
 
-			else
-				render 'new'
-			end
+      else
+        render 'new'
+      end
     end
     
     def update
@@ -59,51 +59,69 @@ class TicketsController < ApplicationController
     
     def destroy
     end
-	
-	def setup
-		session[:tournament_id] = params[:id]
-		redirect_to new_ticket_path
-	end
+  
+  def setup
+    session[:tournament_id] = params[:id]
+    redirect_to new_ticket_path
+  end
 
-  	def check_in
-			@ticket = Ticket.find(params[:id])
-			if (is_current_tournament_organizer @ticket.tournament.id)
-				@ticket.checked_in = true
-				@ticket.save
-				render 'check_in'
-			else
-				render 'ticket_error'
-			end
-		end
-
-  	def payment
-		  @cc = CreditCard.where(person_id: current_person.id)
-  	end
-    
-    def paypal_pay
-      @paypal_noftication = PaypalNotification.create!
+    def check_in
+      @ticket = Ticket.find(params[:id])
+      if (is_current_tournament_organizer @ticket.tournament.id)
+        @ticket.checked_in = true
+        @ticket.save
+        render 'check_in'
+      else
+        render 'ticket_error'
+      end
     end
 
-	private
-		def ticket_params
-			params.require(:ticket).permit(:tickettype)
-		end
+    def payment
+      @cc = CreditCard.where(person_id: current_person.id)
+    end
+        
+    def paypal_pay
+      #double check avalibility and price     
+      @ticket = Ticket.find(params[:id])
+      if @ticket == nil 
+        #ticket shouldn't be nil....
+        redirect_to tournaments_path
+      elsif @ticket.has_paid
+        redirect_to ticket_path(exists_ticket)
+      else 
+        _price = @ticket.get_ticket_price
+        if _price <=0
+          @ticket.has_paid = true
+          @ticket.save
+          redirect_to ticket_path(params[:id])
+        else
+          @ticket.has_paid = false
+          @ticket.save
+          redirect_to @ticket.paypal_url(tournaments_path, @ticket.id, _price)
+        end
+      end
+    end
 
-		def is_current_tournament_organizer (t_id = session[:tournament_id])
-			if TournamentOrganizer.where(tournament_id: t_id, person_id: current_person.id).any?
-				true
-			else
-				false
-			end
-		end
+  private
+    def ticket_params
+      params.require(:ticket).permit(:tickettype)
+    end
 
-		def authenticate_ticket_owner!
-			#check that current user is the owner of this ticket
-			@ticket = Ticket.find(params[:id])
-			@person = current_person
-			if not user_is_admin? and @person != @ticket.person
-				access_denied
-			end
-		end
+    def is_current_tournament_organizer (t_id = session[:tournament_id])
+      if TournamentOrganizer.where(tournament_id: t_id, person_id: current_person.id).any?
+        true
+      else
+        false
+      end
+    end
+
+    def authenticate_ticket_owner!
+      #check that current user is the owner of this ticket
+      @ticket = Ticket.find(params[:id])
+      @person = current_person
+      if not user_is_admin? and @person != @ticket.person
+        access_denied
+      end
+    end
 
 end
